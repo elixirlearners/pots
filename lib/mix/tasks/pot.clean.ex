f34f0e49cdfa841b1abab4e223bc1510f7a4865f
Caleb Gasser <1561693+calebgasser@users.noreply.github.com>
2023-10-28 15:58:11 -0500

    bugfix: when logging info, maps were missing keys

--- lib/mix/tasks/pot.clean.ex
+++ lib/mix/tasks/pot.clean.ex
@@ -58,7 +58,14 @@ defmodule Mix.Tasks.Pot.Clean do
   def stop_docker_containers do
     PotUtils.get_docker_containers()
     |> Enum.each(fn con ->
-      Logger.info("Stopping container: #{List.first(con["Names"])}")
+      case Map.keys(con) do
+        "Names" ->
+          Logger.info("Removing container: #{List.first(con["Names"])}")
+        "Id" -> 
+          Logger.info("Removing container: #{List.first(con["Id"])}")
+        _ -> 
+          Logger.info("Removing container...")
+      end
       PotUtils.stop_container(con)
     end)
   end
@@ -70,7 +77,14 @@ defmodule Mix.Tasks.Pot.Clean do
   def remove_docker_images do
     PotUtils.get_docker_images()
     |> Enum.each(fn img ->
-      Logger.info("Removing image: #{List.first(img["Names"])}")
+      case Map.keys(img) do
+        "Names" -> 
+          Logger.info("Removing image: #{List.first(img["Names"])}")
+        "Id" ->
+          Logger.info("Removing image: #{List.first(img["Id"])}")
+        _ ->
+          Logger.info("Removing image..")
+      end
       PotUtils.remove_image(img)
     end)
   end
