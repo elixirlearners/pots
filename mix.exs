86ba4568c8775fac9830e318c91c6009e44066c6
Caleb Gasser <1561693+calebgasser@users.noreply.github.com>
2023-10-28 15:11:20 -0500

    incremented version number

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Pots.MixProject do
   def project do
     [
       app: :pots,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       description: description(),
